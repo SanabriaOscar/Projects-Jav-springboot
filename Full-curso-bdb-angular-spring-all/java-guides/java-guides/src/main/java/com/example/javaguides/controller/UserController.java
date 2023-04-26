6b3b6df0cfe0bed5139fe5938fa6fee13dee51ab
Oscar Jesus Sanabria Tavera <52457473+SanabriaOscar@users.noreply.github.com>
2023-04-25 22:23:36 -0500

    subiendo mis primero pasos en angular

---
+++ Full-curso-bdb-angular-spring-all/java-guides/java-guides/src/main/java/com/example/javaguides/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.example.javaguides.controller;
+
+import com.example.javaguides.model.User;
+import com.example.javaguides.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("api/")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("users")
+    public List<User> getUsers() {
+        return this.userRepository.findAll();
+    }
+}
+
