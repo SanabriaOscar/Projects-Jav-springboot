4909744b950e4b61ce2ee03f6fae2fe4683bf19f
Oscar Jesus Sanabria Tavera <52457473+SanabriaOscar@users.noreply.github.com>
2023-11-22 16:08:05 -0500

    se sube segunda parte de finanzen

---
+++ FINNAN-ZEN-SpringBoot-Angular/2-Finanzen-entitys/Finanzen-api-backed/src/main/java/com/example/springbootapirestbacked/repository/ClienteRepository.java
@@ -0,0 +1,17 @@
+package com.example.springbootapirestbacked.repository;
+
+import com.example.springbootapirestbacked.model.Cliente;
+import com.example.springbootapirestbacked.model.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente,Long> {
+ @Query("from Region ")
+    public List<Region>findAllRegiones();
+}
+
+
